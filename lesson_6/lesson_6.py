86db6d5b7b353862e9f453c3bd896943999ccab8
JeeFoxx <a.i.guminskiy@gmail.com>
2024-11-01 12:21:44 +0500

    step_8 Присвойте паролю рейтинг

--- lesson_6/lesson_6.py
+++ lesson_6/lesson_6.py
@@ -1,11 +1,11 @@
 input_pass = input("Введите пароль: ")
-# answer = len(input_pass)
+score = 0
 
 def has_digit(password):
     for symbol in password:
         if symbol.isdigit():
             return True
-        return False
+    return False
 
 def is_very_long(password):
     length = len(password)
@@ -13,6 +13,17 @@ def is_very_long(password):
         return False
     return True
 
+if has_digit(input_pass) == True:
+    score += 2
+else:
+    score += 0
+
+if is_very_long(input_pass) == True:
+    score +=2
+else:
+    score +=0
+
+print(f"Рейтинг пароля: {score}")
 
 
 
--- lesson_6/test.py
+++ lesson_6/test.py
@@ -1,13 +1,13 @@
-# def check_number_in_pass(password):
-#     for symbol in password:
-#         if symbol.isdigit():
-#             return True
-#     return False
-# print(check_number_in_pass("лы12овис"))
+def check_number_in_pass(password):
+    for symbol in password:
+        if symbol.isdigit():
+            return True
+    return False
+print(check_number_in_pass("лы12овис"))
 
-def is_very_long(password):
-    length = len(password)
-    if length < 12:
-        return False
-    return True
+# def is_very_long(password):
+#     length = len(password)
+#     if length < 12:
+#         return False
+#     return True
 print(is_very_long("kjgfhsgd"))
